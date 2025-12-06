14f8d9b113dc464f46a45144934d107256120611
Bruno <brunoep.es@gmail.com>
2025-12-06 18:16:07 -0500

    modificaciones react

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Advertencia: Esto permite que el build termine incluso si hay errores de ESLint.
-    ignoreDuringBuilds: true,
-  },
+
   typescript: {
     // Advertencia: Esto permite que el build termine incluso si hay errores de TypeScript.
     ignoreBuildErrors: true,
